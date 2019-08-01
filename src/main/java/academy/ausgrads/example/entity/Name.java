151e792089b4ff6049d3ad2e09f07346a2abab2c
798021 <798021@CTSAUS-LT-487.cts.com>
2019-08-01 11:13:02 +1000

    Just changes for Learning

--- src/main/java/academy/ausgrads/example/entity/Name.java
+++ src/main/java/academy/ausgrads/example/entity/Name.java
@@ -8,26 +8,27 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "NAME")
 public class Name {
-  @Id
-  @Column(name = "NAME_ID")
-  private Long id;
+	@Id
+	@Column(name = "NAME_ID")
+	private Long id;
 
-  @Column(name = "NAME")
-  private String name;
+	@Column(name = "NAME")
+	private String name;
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 }
+//adding comment just to see GitHub changes
